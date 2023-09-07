c8a1f97bc41ab9696c442930270cba2216b1aa93
Rafael <rafaelhibiki@gmail.com>
2023-09-07 03:01:10 -0300

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import Canvas from "./../canvas/index";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <h1 className="head-text">React App</h1>
-    </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
